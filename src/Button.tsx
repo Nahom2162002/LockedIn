16c8df84c0e1a89e9732f5232236195756bea4aa
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-04-26 22:39:35 -0400

    Button component

---
+++ src/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function Button() {
+    const [state, setState] = useState(0);
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Button;
