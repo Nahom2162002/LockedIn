c0838e3382d07f191c666865716b970eb1dd7cb8
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-04 23:26:26 -0400

    Fixed issue with link

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import './App.css';
 import Menu from './Menu.tsx';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className='matrixBackground'>
         <h1>Welcome to LockedIn!</h1>
-        <Link to="/menu">Click here to get started</Link>
+        <a href={chrome.runtime.getURL("index.html#/menu")} target="_blank" rel="noreferrer">Click here to get started</a>
       </div>
-      
       <Routes>
         <Route path="/menu" element={<Menu/>}/>
       </Routes>
-    </BrowserRouter> 
-  )
+    </HashRouter> 
+  );
 }
 
 export default App;
