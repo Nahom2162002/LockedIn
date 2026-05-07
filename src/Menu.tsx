1fb8d7fb167a390b630e5f77d7e9cb3e9c714afc
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-07 16:21:41 -0400

    Testing animations

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,7 +1,9 @@
 function Menu() {
     return (
         <div className="menuBackground">
-
+            <div className="websiteChoices">
+                <h1 id="hello">Hello</h1>
+            </div> 
         </div>
     );
 }
