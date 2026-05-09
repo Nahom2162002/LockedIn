d3a615e676adc4b46173cfaba36e0c885ad3a2d7
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-08 21:34:54 -0400

    Changed background photo + added mini menu for user to add websites to restrict

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,8 +1,12 @@
 function Menu() {
     return (
         <div className="menuBackground">
-            <div className="websiteChoices">
+            <div>
                 <h1 id="hello">Hello</h1>
+                <div className="websiteChoices">
+                    <h3>Please add websites you would like to restrict</h3>
+                    
+                </div>
             </div> 
         </div>
     );
