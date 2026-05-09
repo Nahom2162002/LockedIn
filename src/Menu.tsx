f5e75944bd11a782e5f5f7e9a6949ef3d9f2feda
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-09 19:05:58 -0400

    Added text input and Add button

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,7 +2,6 @@ import RestrictionInfo from './RestrictionInfo.tsx';
 import { useState } from 'react';
 
 function Menu() {
-
     const [isOpen, setIsOpen] = useState(false);
 
     return (
@@ -11,7 +10,7 @@ function Menu() {
                 <h1 id="hello">Hello</h1>
                 <div className="websiteChoices">
                     <h3>Please add websites you would like to restrict</h3>
-                    <button onClick={() => setIsOpen(true)}>Add</button>
+                    <button onClick={() => setIsOpen(true)}>+</button>
                     {isOpen && <RestrictionInfo onClose={() => setIsOpen(false)}/>}
                 </div>
             </div> 
--- src/RestrictionInfo.tsx
+++ src/RestrictionInfo.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 function RestrictionInfo({ onClose }: {onClose: () => void}) {
+    const [text, setText] = useState('');
+
     return (
         <div className="sitechoicebackground">
-            <h1>Hello</h1>
-            <button onClick={onClose}>Close</button>
+            <h3 id="websiteinfo">Website information</h3>
+            <p id="url">URL:</p>
+            <input id="urltext" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter URL here"/>
+            <button className="closebutton" onClick={onClose}>Add</button>
         </div>
     );
 }
