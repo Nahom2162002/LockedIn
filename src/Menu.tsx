769fdc57bf23830b64dd75d7bf89b5f98d47124f
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-08 23:38:41 -0400

    Currently implementing mini menu and adding button to direct user to menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,11 +1,22 @@
+import RestrictionInfo from './RestrictionInfo.tsx';
+import { useState } from 'react';
+
 function Menu() {
+
+    const [isOpen, setIsOpen] = useState(true);
+
     return (
         <div className="menuBackground">
             <div>
                 <h1 id="hello">Hello</h1>
                 <div className="websiteChoices">
                     <h3>Please add websites you would like to restrict</h3>
-                    
+                    <button onClick={RestrictionInfo}>Add</button>
+                    {isOpen && (
+                        <div>
+                            <button onClick={() => setIsOpen(false)}>Close</button>
+                        </div>
+                    )}
                 </div>
             </div> 
         </div>
