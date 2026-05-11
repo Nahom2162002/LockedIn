dfe75cad3796d68aca4f6fd88adee633f387ab4b
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-10 21:05:14 -0400

    Added date + time feature and styled elements

--- src/RestrictionInfo.tsx
+++ src/RestrictionInfo.tsx
@@ -2,12 +2,18 @@ import { useState } from 'react';
 
 function RestrictionInfo({ onClose }: {onClose: () => void}) {
     const [text, setText] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
 
     return (
         <div className="sitechoicebackground">
             <h3 id="websiteinfo">Website information</h3>
             <p id="url">URL:</p>
             <input id="urltext" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter URL here"/>
+            <input id="datetext" type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+            <p id="date">Date:</p>
+            <input id="timetext" type="time" value={time} onChange={(e) => setTime(e.target.value)}/>
+            <p id="time">Time:</p>
             <button className="closebutton" onClick={onClose}>Add</button>
         </div>
     );
