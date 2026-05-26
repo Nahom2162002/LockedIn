afa20c1078585ecb205ab910b4913f8034161c30
Nahom2162002 <NahomAshagrea2002@gmail.com>
2026-05-26 13:54:46 -0400

    Fixed styling with login page + adding functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Menu from './Menu.tsx';
+import Login from './Login.tsx';
+import CreateAccount from './CreateAccount.tsx';
+import ForgotPassword from './ForgotPassword.tsx';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function Home() {
   return (
     <div className='matrixBackground'>
         <h1>Welcome to LockedIn!</h1>
-        <a href={chrome.runtime.getURL("index.html#/menu")} target="_blank" rel="noreferrer">Click here to get started</a>
+        <a href={chrome.runtime.getURL("index.html#/login")} target="_blank" rel="noreferrer">Click here to get started</a>
     </div>
   );
 }
@@ -16,7 +19,10 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/menu" element={<Menu/>}/>
+        <Route path="/create" element={<CreateAccount/>}/>
+        <Route path="/forgot" element={<ForgotPassword/>}/>
       </Routes>
     </HashRouter> 
   );
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+import LockIcon from './assets/lockIcon.png';
+//import ForgotPassword from './ForgotPassword.tsx';
+//import CreateAccount from './CreateAccount.tsx';
+
 function Login() {
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <div className="login-background">
-
+            <div className="login">
+                <img src={LockIcon} id="lock-icon"/>
+                <input id="usernametext" type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='Username'/>
+                <input id="passwordtext" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password'/>
+                <input id="loginbutton" type="button" placeholder='Log in'/>
+                <input id="createaccountbutton" type="button" placeholder='Create Account'/>
+            </div>
         </div>
     );
 }
